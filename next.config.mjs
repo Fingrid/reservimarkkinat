481c4b1ea0fa5615ac822b41650d2fb328cb4ec9
Jaakko Rytinki <jaakko.rytinki@gmail.com>
2025-01-27 08:48:02 +0200

    fix: typo in path

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
-  assetPrefix: isProd ? '/resurssimarkkinat-dev/' : '',
-  basePath: isProd ? '/resurssimarkkinat-dev/' : '',
+  assetPrefix: isProd ? '/resurssimarkkinat-dev' : '',
+  basePath: isProd ? '/resurssimarkkinat-dev' : '',
   images: {
     unoptimized: true // mandatory, otherwise won't export
   }
