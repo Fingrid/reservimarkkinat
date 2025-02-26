3963c6cf2dfd9aa2c6a82b9e0420a6b2fc379e5a
Ilkka Hulkko <61559521+hulkko@users.noreply.github.com>
2025-02-26 08:17:21 +0200

    Update _meta.ts

--- content/_meta.ts
+++ content/_meta.ts
@@ -3,6 +3,7 @@ export default {
     '0###': { type: 'separator', title: 'General' },
     market_platforms: 'Market platforms',
     information_exchange: 'Information exchange',
+    about: 'About',
     '1###': { type: 'separator', title: 'Market Platforms' },
     mFRR_C: 'Balancing capacity markets (mFRR)',
     mFRR_E: 'Balancing energy markets (mFRR)',
