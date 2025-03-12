9ed089517723f9972577a0dd6f6aae1f0f33a1c5
Jaakko Rytinki <390991+destor@users.noreply.github.com>
2025-03-12 12:46:22 +0200

    Update Next.JS config to reflect the new domain

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
-  assetPrefix: isProd ? '/reservimarkkinat' : '',
-  basePath: isProd ? '/reservimarkkinat' : '',
+  //assetPrefix: isProd ? '/reservimarkkinat' : '',
+  //basePath: isProd ? '/reservimarkkinat' : '',
   images: {
     unoptimized: true // mandatory, otherwise won't export
   }
