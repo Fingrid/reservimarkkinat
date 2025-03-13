709793ff6c8bcc25b9462ab7bac39cf67421a72a
Ilkka Hulkko <61559521+hulkko@users.noreply.github.com>
2025-03-13 15:32:23 +0200

    Update _meta.ts

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,15 +1,60 @@
 export default {
-    index: { type: 'page', title: 'Developer portal'},
-    '0###': { type: 'separator', title: 'General' },
-    market_platforms: { type: 'page', title: 'Market platforms' },
-    information_exchange: { type: 'page', title: 'Information exchange' },
-    cim: { type: 'page', title: 'Common Information Model - CIM' },
-    about: { type: 'page', title: 'About' },
-    '1###': { type: 'separator', title: 'Market Platforms' },
-    mFRR_C: { type: 'page', title: 'Balancing capacity markets (mFRR)', display: 'hidden' },
-    mFRR_E: { type: 'page', title: 'Balancing energy markets (mFRR)', display: 'hidden'  },
-    aFRR_C: { type: 'page', title: 'Automatic frequency restoration reserve capacity markets (aFRR)', display: 'hidden'  },
-    aFRR_E: { type: 'page', title: 'Automatic frequency restoration reserve energy markets (aFRR)' },
-    FCR: { type: 'page', title: 'Frequency containment reserves (FCR products)', display: 'hidden'  }, 
-    FFR: { type: 'page', title: 'Fast Frequency Reserve (FFR)', display: 'hidden'  },
+    index: { 
+        type: 'page', 
+        title: 'Developer portal'
+    },
+    '0###': { 
+        type: 'separator', 
+        title: 'General'
+    },
+    market_platforms: { 
+        type: 'page', 
+        title: 'Market platforms' 
+    },
+    information_exchange: { 
+        type: 'page', 
+        title: 'Information exchange' 
+    },
+    cim: { 
+        type: 'page', 
+        title: 'Common Information Model - CIM' 
+    },
+    about: { 
+        type: 'page', 
+        title: 'About',
+        display: 'hidden'        
+    },
+    '1###': { 
+        type: 'separator', 
+        title: 'Market Platforms' 
+    },
+    aFRR_E: { 
+        type: 'page', 
+        title: 'Automatic frequency restoration reserve energy markets (aFRR)' 
+    },
+    mFRR_C: { 
+        type: 'page', 
+        title: 'Balancing capacity markets (mFRR)', 
+        display: 'hidden' 
+    },
+    mFRR_E: { 
+        type: 'page', 
+        title: 'Balancing energy markets (mFRR)',
+        display: 'hidden'  
+    },
+    aFRR_C: {
+        type: 'page', 
+        title: 'Automatic frequency restoration reserve capacity markets (aFRR)', 
+        display: 'hidden'  
+    },
+    FCR: { 
+        type: 'page', 
+        title: 'Frequency containment reserves (FCR products)', 
+        display: 'hidden'  
+    }, 
+    FFR: { 
+        type: 'page', 
+        title: 'Fast Frequency Reserve (FFR)', 
+        display: 'hidden' 
+    }
   }
