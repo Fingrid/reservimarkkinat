29f411417493f1117eadac98b8d29da02eeb0ffe
Jaakko Rytinki <jaakko.rytinki@gmail.com>
2025-04-10 13:15:51 +0300

    chore added nextjs eslint plugin

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,11 +5,25 @@ import tseslint from "typescript-eslint";
 // @ts-ignore
 import eslintConfigPrettier from "eslint-config-prettier";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import pluginNext from "@next/eslint-plugin-next";
+
 export default [
   {
     ignores: [".next/**/*", ".obsidian/**/*", "node_modules/**/*", "out/**/*"],
   },
   ...tseslint.config(
+    {
+      plugins: {
+        "@next/next": pluginNext,
+      },
+      files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+      rules: {
+        ...pluginNext.configs.recommended.rules,
+        ...pluginNext.configs["core-web-vitals"].rules,
+      },
+    },
     eslint.configs.recommended,
     tseslint.configs.strict,
     eslintConfigPrettier,
