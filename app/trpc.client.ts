0c29a86e811fa1f820e0332eae294f2bf8aebbc7
Jaakko Rytinki <jaakko.rytinki@gmail.com>
2025-06-10 00:01:48 +0300

    chore: debugging

--- app/trpc.client.ts
+++ app/trpc.client.ts
@@ -1,11 +1,18 @@
-import { getUntypedClient, createTRPCClient, httpLink } from "@trpc/client";
+import { getUntypedClient, createTRPCClient, loggerLink, httpBatchLink } from "@trpc/client";
 
 const env = process.env;
 const VALIDATOR_ENDPOINT = env.VALIDATOR_ENDPOINT || "http://localhost:8787/trpc";
+//const DEBUG_ENABLED = env.DEBUG_ENABLED === "true";
 
 const anyTypeClient = createTRPCClient({
   links: [
-    httpLink({
+    loggerLink(
+      {
+        enabled: () => true,
+      }
+    ),
+    httpBatchLink({
+      maxItems: 5,
       url: VALIDATOR_ENDPOINT
     })
   ]
